bb0b0d1766949e8afb23854cb083855ead50b3f3
michael <michael.burzan@outlook.de>
2023-08-21 10:18:27 +0200

    add files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { NativeScriptRouterModule } from '@nativescript/angular'
 
 import { ItemsComponent } from './item/items.component'
 import { ItemDetailComponent } from './item/item-detail.component'
+import { TableChartComponent } from './tablechart/tablechart.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/items', pathMatch: 'full' },
-  { path: 'items', component: ItemsComponent },
-  { path: 'item/:id', component: ItemDetailComponent },
+  //{ path: '', redirectTo: '/items', pathMatch: 'full' },
+  //{ path: 'items', component: ItemsComponent },
+  //{ path: 'item/:id', component: ItemDetailComponent },
+  { path: '', redirectTo: '/tablechart', pathMatch: 'full' },
+  { path: 'tablechart', component: TableChartComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
-import { NativeScriptModule } from '@nativescript/angular'
+import { NativeScriptModule, NativeScriptFormsModule } from '@nativescript/angular'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { ItemsComponent } from './item/items.component'
 import { ItemDetailComponent } from './item/item-detail.component'
+import { TableChartComponent } from './tablechart/tablechart.component'
+import { BookingDetailsDialogComponent } from './bookingdetaildialog/booking-details-dialog.component'
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
-  declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
+  imports: [NativeScriptModule, AppRoutingModule, NativeScriptFormsModule],
+  declarations: [AppComponent, ItemsComponent, ItemDetailComponent, TableChartComponent, BookingDetailsDialogComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
+  entryComponents: [
+        BookingDetailsDialogComponent
+    ]
 })
 export class AppModule {}
